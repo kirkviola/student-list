7e8987f5037dccf39c092a33680ec13f15c561ec
Matthew Kirkendall <kirk.viola@gmail.com>
2021-11-16 12:58:04 -0500

    Added binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<h1>Student List!</h1>',
+  templateUrl: './app.component.html',
   styles: ["h1 { color: red; } "]
 })
 export class AppComponent implements OnInit {
   title = 'student-list';
+  name: string = "";
 
+  clicked(): void {
+    this.title = "Fred's student-list";
+  }
   ngOnInit() : void {
     console.debug("This is a debug message");
   }
